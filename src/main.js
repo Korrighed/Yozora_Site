4da53c1dc549da523f837e913500cb28bbe83af4
Alex <boutalexis@gmail.com>
2025-12-28 20:14:35 +1100

    Fix/Import composant twitch

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './styles/main.scss'
 import { TimezoneBlock } from './components/TimezoneBlock.js'
 import { ImageAnimation } from './components/Animation.js'
-import { TwitchIntegration } from './twitch-integration.js';
+import { TwitchIntegration } from './components/TwitchIntegration.js';
 
 // --------------- Style management
 // Apply the yozora-container class to the presentation section
